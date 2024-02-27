7ebbb0cf3b877cd747043fd6b2d5ed41ff7b0da5
eitay <alter1eitai@gmail.com>
2024-02-27 14:46:11 +0200

    adding all Identity

--- TheLaptopStore/Data/ApplicationDbContext.cs
+++ TheLaptopStore/Data/ApplicationDbContext.cs
@@ -3,11 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace TheLaptopStore.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        public DbSet<Laptop> Laptops { get; set; }
+        public DbSet<CreditCard> CreditCards { get; set; }
     }
 }
---
+++ TheLaptopStore/Data/CreditCard.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheLaptopStore.Data
+{
+    [Table("CreditCard")]
+    public class CreditCard
+    {
+        [Key]
+        [Required]
+        [RegularExpression("^d{16}$", ErrorMessage = "Credit card number must be 16 digits only")]
+        public int Number { get; set; }
+
+        [Required]
+        public string ExpDate { get; set; }
+
+        [Required]
+        [RegularExpression("^d{3}$", ErrorMessage = "CVV must be 3 digits only")]
+        public int CVV { get; set; }
+
+        [Required]
+        [RegularExpression("^[0-9]{9}$", ErrorMessage = "ID must be 9 numbers long")]
+        public string PersonalID { get; set; }
+    }
+}
