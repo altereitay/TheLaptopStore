02e55c39f37c73599dbee60dbea1990bd9bb1cab
nir kanimach <nironi1998@gmail.com>
2024-03-03 19:39:07 +0200

    Finish add and remove products in admin page

--- TheLaptopStore/Controllers/AdminPage.cs
+++ TheLaptopStore/Controllers/AdminPage.cs
@@ -1,5 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using TheLaptopStore.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+
+
 
 namespace TheLaptopStore.Controllers
 {
@@ -34,7 +53,7 @@ namespace TheLaptopStore.Controllers
             _db.Laptops.Remove(obj);
             _db.SaveChanges();
             List<Laptop> laptops = _db.Laptops.ToList();
-            return View("AdminPage", laptops);
+            return View("AdminPage");
         }
 
         public IActionResult deleteProduct()
@@ -48,6 +67,73 @@ namespace TheLaptopStore.Controllers
         {
             return View("addProduct");
         }
+
+        public IActionResult addProduct()
+        {
+  
+            if (ModelState.IsValid)
+            {
+                Laptop laptop = new Laptop();
+                laptop.Ram = Convert.ToInt32(Request.Form["Ram"]);
+                laptop.SSD = Convert.ToInt32(Request.Form["SSD"]);
+                laptop.CPU = Request.Form["CPU"];
+                laptop.ScreenSize = Convert.ToDouble(Request.Form["ScreenSize"]);
+                laptop.GPU = Request.Form["GPU"];
+                laptop.Price = Convert.ToInt32(Request.Form["Price"]);
+                laptop.Maker = Request.Form["Maker"];
+                laptop.Color = Request.Form["Color"];
+                laptop.Quantity = Convert.ToInt32(HttpContext.Request.Form["Quantity"]);
+                laptop.Description = Request.Form["Description"];
+                laptop.Picture = Request.Form["Picture"];
+                laptop.IsOnSale = Request.Form["IsOnSale"].Count > 0 ? true : false;
+                laptop.SalePrecentage = Convert.ToInt32(Request.Form["SalePrecentage"]);
+                laptop.PopularityIndex = Convert.ToInt32(HttpContext.Request.Form["PopularityIndex"]);
+                laptop.Category = Request.Form["Category"];
+                laptop.ReleaseDate = Request.Form["ReleaseDate"];
+                laptop.Model = Request.Form["Model"];
+                _db.Laptops.Add(laptop);
+                _db.SaveChanges();
+                return View("AdminPage");
+            }
+            else
+            {
+                return View("addProduct");
+            }
+            
+        }
+        public IActionResult ShowdeleteUser()
+        {
+            List<ApplicationUser> users = _db.Users.ToList();
+            return View("deleteUser", users);
+        }
+        public IActionResult deleteUser(string Id)
+        {
+            var obj = _db.Users.Find(Id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Users.Remove(obj);
+            _db.SaveChanges();
+            return View("AdminPage");
+        }
+        public IActionResult showAddAdmin() { 
+        
+                return  View("addAdmin");
+        }
+
+      
+
+        /*public IActionResult addAdmin()
+        {
+     
+
+
+
+        }
+        */
 
     }
 }
