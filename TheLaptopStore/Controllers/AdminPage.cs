269601122742a309c755e8c9775c2f22ef6de831
nir kanimach <nironi1998@gmail.com>
2024-03-03 12:01:02 +0200

    Delete laptop as admin

---
+++ TheLaptopStore/Controllers/AdminPage.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheLaptopStore.Data;
+
+namespace TheLaptopStore.Controllers
+{
+    public class AdminPage : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+        public AdminPage(ILogger<HomeController> logger, ApplicationDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+     
+        public IActionResult Managment()
+        {
+            List<Laptop> laptops = _db.Laptops.ToList();
+            return View("AdminPage",laptops);
+        }
+
+
+        
+        public IActionResult Delete(string? model)
+        {
+            var obj = _db.Laptops.Find(model);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Laptops.Remove(obj);
+            _db.SaveChanges();
+            List<Laptop> laptops = _db.Laptops.ToList();
+            return View("AdminPage", laptops);
+        }
+    }
+}
