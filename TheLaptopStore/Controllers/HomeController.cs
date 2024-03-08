9b30afec43623e7d6f8a8d8a5f015aaf72949dc1
nir kanimach <nironi1998@gmail.com>
2024-03-08 17:52:18 +0200

    Filter_Product_Part1

--- TheLaptopStore/Controllers/HomeController.cs
+++ TheLaptopStore/Controllers/HomeController.cs
@@ -33,6 +33,35 @@ namespace TheLaptopStore.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult FilterProducts(List<int> ramFilter, List<string> cpuFilter, List<string> gpuFilter)
+        {
+            IQueryable<Laptop> filteredProducts = _db.Laptops.AsQueryable();
+
+         
+            // Apply RAM filter
+            if (ramFilter != null && ramFilter.Any())
+            {
+                filteredProducts = filteredProducts.Where(laptop => ramFilter.Contains(laptop.Ram));
+            }
+
+            // Apply CPU filter
+            if (cpuFilter != null && cpuFilter.Any())
+            {
+                filteredProducts = filteredProducts.Where(laptop => cpuFilter.Contains(laptop.CPU));
+            }
+
+            // Apply GPU filter
+            if (gpuFilter != null && gpuFilter.Any())
+            {
+                filteredProducts = filteredProducts.Where(laptop => gpuFilter.Contains(laptop.GPU));
+            }
+
+            // Execute the query and retrieve the filtered products
+            List<Laptop> result = filteredProducts.ToList();
+
+            // Pass the filtered products to the view
+            return View("Index",result);
+        }
 
 
     }
